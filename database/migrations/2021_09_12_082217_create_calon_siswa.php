fa05a720b14ca05fd2ada800bb992e63891d917e
techinfo5083 <techinfo5083@gmail.com>
2021-09-12 16:09:58 +0700

    Buat migration jurusan dan calon siswa

---
+++ database/migrations/2021_09_12_082217_create_calon_siswa.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalonSiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calon_siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisn');
+            $table->string('jk',2);
+            $table->string('tempat_lahir', 50);
+            $table->string('alamat', 100);
+            $table->date('tgl_lahir');
+            $table->string('nama_sekolah', 50);
+            $table->integer('id_jurusan');
+            $table->string('no_telp', 13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calon_siswa');
+    }
+}
